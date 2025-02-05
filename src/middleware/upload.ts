95f7161de8a1ba65c7d1f38ae5ea8babeb38ceb8
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-05 21:52:29 +0000

    feat: add upload middleware

---
+++ src/middleware/upload.ts
@@ -0,0 +1,21 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../storage"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Just images are allowed!"), false);
+  }
+};
+
+export const upload = multer({ storage, fileFilter });
