e44a8724f97282ecc359fbbaf3f672885be8d18d
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-05 22:33:38 +0000

    feat: add ProjectController

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Project } from "../models/Project";
+import { Tag } from "../models/Tag";
+
+export class ProjectController {
+  async getAllProjects(req: Request, res: Response) {
+    const projectRepository = getRepository(Project);
+    const projects = await projectRepository.find({ relations: ["tags"] });
+    res.json(projects);
+  }
+
+  async createProject(req: Request, res: Response) {
+    const { title, repoUrl, serviceUrl, isBackend, tags } = req.body;
+    const imageUrl = req.file ? req.file.filename : "";
+
+    const projectRepository = getRepository(Project);
+    const tagRepository = getRepository(Tag);
+
+    const tagEntities = await Promise.all(
+      tags.map(async (tagName: string) => {
+        let tag = await tagRepository.findOne({ where: { name: tagName } });
+        if (!tag) {
+          tag = tagRepository.create({ name: tagName });
+          await tagRepository.save(tag);
+        }
+        return tag;
+      })
+    );
+
+    const project = projectRepository.create({
+      title,
+      imageUrl,
+      repoUrl,
+      serviceUrl,
+      isBackend,
+      tags: tagEntities,
+    });
+
+    await projectRepository.save(project);
+    res.status(201).json(project);
+  }
+}
