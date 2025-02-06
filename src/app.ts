3800450251251d661bc7e94588b0b5f0e9edfc46
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-06 23:44:49 +0000

    feat: add routes to app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Express, Response, Request } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import projectRoutes from "./routes/projectRoutes";
+import tagRoutes from "./routes/tagRoutes";
+
 dotenv.config();
 
 const app: Express = express();
@@ -12,5 +15,8 @@ app.use(express.json());
 app.get("/", (_req: Request, res: Response) => {
   res.send("welcome to the API of my Portfolio");
 });
+
+app.use("/api", projectRoutes);
+app.use("/api", tagRoutes);
 
 export { app };
