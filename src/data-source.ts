d384e1e0f34759a500fde4c6285dbcc4812a03b3
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-07 00:58:28 +0000

    feat: update typeORM config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,36 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { Project } from "./models/Project";
-import { Tag } from "./models/Tag";
-import { User } from "./models/User";
+
+if (!process.env.DB_TYPE) {
+  throw new Error("DB_TYPE environment variable not set.");
+}
+
+if (!process.env.DB_HOST) {
+  throw new Error("DB_HOST environment variable not set.");
+}
+
+if (!process.env.DB_PORT) {
+  throw new Error("DB_PORT environment variable not set.");
+}
+
+if (!process.env.DB_USERNAME) {
+  throw new Error("DB_USERNAME environment variable not set.");
+}
+
+if (!process.env.DB_PASSWORD) {
+  throw new Error("DB_PASSWORD environment variable not set.");
+}
+
+if (!process.env.DB_DATABASE) {
+  throw new Error("DB_DATABASE environment variable not set.");
+}
+
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET environment variable not set.");
+}
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  type: process.env.DB_TYPE,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
@@ -13,7 +38,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_DATABASE,
   synchronize: true,
   logging: true,
-  entities: [Project, Tag, User],
+  entities: ["src/models/*.ts"],
   subscribers: [],
   migrations: [],
 });
