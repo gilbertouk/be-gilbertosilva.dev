a15af0ad33bc8af57233877dddba34a5e55c3f53
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-08 04:09:02 +0000

    feat: update auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,25 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET environment variable not set.");
+}
+
 export const authMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): void => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
-    return res
-      .status(401)
-      .json({ message: "Access denied. No token provided." });
+    res.status(401).json({ message: "Access denied. No token provided." });
+    return;
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
+    jwt.verify(token, process.env.JWT_SECRET as string);
+    return next();
   } catch (error) {
     console.error("Invalid token:", error);
     res.status(400).json({ message: "Invalid token." });
+    return;
   }
 };
