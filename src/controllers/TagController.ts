717cb9db90fc4555a34aa10311cb2ea4061a0fd6
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-08 04:08:33 +0000

    feat: update TagController

--- src/controllers/TagController.ts
+++ src/controllers/TagController.ts
@@ -1,36 +1,47 @@
-import { Request, Response } from "express";
-// import { getRepository } from "typeorm";
+import { NextFunction, Request, Response } from "express";
 import { Tag } from "../models/Tag";
 import { AppDataSource } from "../data-source";
 
 export class TagController {
-  async getAllTags(req: Request, res: Response) {
-    const tagRepository = AppDataSource.getRepository(Tag);
-    const tags = await tagRepository.find();
-    res.json(tags);
-  }
-
-  async createTag(req: Request, res: Response) {
-    const { name } = req.body;
-    const tagRepository = AppDataSource.getRepository(Tag);
-
-    const tag = tagRepository.create({ name });
-    await tagRepository.save(tag);
-    res.status(201).json(tag);
-  }
-
-  async updateTag(req: Request, res: Response) {
-    const { id } = req.params;
-    const { name } = req.body;
-    const tagRepository = AppDataSource.getRepository(Tag);
-
-    const tag = await tagRepository.findOne(id);
-    if (!tag) {
-      return res.status(404).json({ message: "Tag não encontrada" });
+  async getAllTags(req: Request, res: Response, next: NextFunction) {
+    try {
+      const tagRepository = AppDataSource.getRepository(Tag);
+      const tags = await tagRepository.find();
+      res.json(tags);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    tag.name = name;
-    await tagRepository.save(tag);
-    res.json(tag);
+  async createTag(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name } = req.body;
+      const tagRepository = AppDataSource.getRepository(Tag);
+      const tag = tagRepository.create({ name });
+      await tagRepository.save(tag);
+      res.status(201).json(tag);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateTag(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = Number(req.params.id);
+      const { name } = req.body;
+      const tagRepository = AppDataSource.getRepository(Tag);
+
+      const tag = await tagRepository.findOne({ where: { id } });
+      if (!tag) {
+        res.status(404).json({ message: "Tag not found" });
+        return;
+      }
+
+      tag.name = name;
+      await tagRepository.save(tag);
+      res.json(tag);
+    } catch (error) {
+      next(error);
+    }
   }
 }
